448fffc6aba8bca2b1d8dabeeeb7325f2544fef3
LuisinRosa <j.luis7878@hotmail.com>
2016-11-19 13:59:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express=require('express'); 
 var app=express();
-var http=require('http').server(app);
+var http=require('http').Server(app);
 var io=require('socket.io')(http);
 
 app.use(express.static(__dirname +  '/public'));//aqui se serviran archivos estaticos
