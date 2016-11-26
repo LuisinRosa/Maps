7553ff117040390d50aa88040f42b09108802737
LuisinRosa <j.luis7878@hotmail.com>
2016-11-26 09:48:28 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express=require('express'); 
-var app=express();
-var http=require('http').Server(app);
-var io=require('socket.io')(http);
+var express= require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static(__dirname +  '/public'));//aqui se serviran archivos estaticos
 
-//le decimos en que puerto se ejecutara
-var port=process.env.PORT || 3000;//esto lo exige heroku
+app.use(express.static(__dirname + '/public')); //serving statics files like css, js, images
 
-//cuando alguien haga una peticion get al sitio, redireccionara al index
-app.get('/',function(req,res) {
-	res.sendFile(__dirname+'/index.html');//ruta para index
+var port=process.env.PORT || 3000; //this is for heroku
+
+app.get('/', function(req, res){
+
+  res.sendFile(__dirname + '/index.html');
+
 });
-
 http.listen(port,function(){
-	console.log('Escuchando en el puerto: '+port);
+    console.log('Escuchando en el puerto: '+port);//mensaje en consola 
 });
